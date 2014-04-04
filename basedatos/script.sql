e7542d47b49aec87161efdd25baf27c08292f517
Henry Miguel Guzmán Escorcia <hguzman10@gmail.com>
2014-04-03 20:47:30 -0500

    Inicio de sesion

---
+++ basedatos/script.sql
@@ -0,0 +1,10 @@
+create database senatga;
+
+use senatga;
+
+create table usuarios(
+usuario varchar(20),
+contrasena varchar(20),
+primary key (usuario));
+
+insert into usuarios value("hguzman","12345");
--- login.php
+++ login.php
@@ -1,6 +1,23 @@
 <?php
+
 	$usuario=$_POST['usu'];
 	$contrasena=$_POST['con'];
+
+	$db = mysql_connect("127.0.0.1", "root","usrio01"); // Conecta la base de datos
+	mysql_select_db("senatga",$db);// Seleccion la base de datos correcta
+
+	$sql = "select * from usuarios where usuario='$usuario' and contrasena='$contrasena'";
+
+	$result = mysql_query($sql);
+
+	$contador=  mysql_num_rows($result);
+
+	if ($contador<1){
+		header('Location: index.html');
+	}
+		session_start(); //Inicia Manipulación de Variables de Session
+	    $_SESSION["hola"]=$usuario;
+
 ?>
 
 <!doctype html>
@@ -24,23 +41,28 @@
 				<section class="top-bar-section">
 				    <!-- Right Nav Section -->
 				    <ul class="right">				      
-				      <li class="active"><a href="#"><?php echo "USUARIO ".$usuario ?></a></li>
+				      <li class="active"><a href="cerrarsesion.php">Cerrar Sesión</a></li>
 				    </ul>
-				  </section>			    			
+				<!-- Left Nav Section -->
+				    <ul class="left">
+				      <li><a href="#"><?php echo $usuario ?></a></li>
+				    </ul>
+				</section>			    			
 			  </nav>
 		</div>
 		<br>
 		<div class="row">
-			<div class="large-4 columns">
+			<div class="large-2 columns panel">
 				<ul class="side-nav">
-				  <li><a href="#">Modulo 1</a></li>
+				  <li><a href="#">Usuario</a></li>
 				  <li><a href="#">Modulo 2</a></li>
 				  <li><a href="#">Modulo 3</a></li>
 				  <li><a href="#">Modulo 4</a></li>
 				</ul>				
 			</div>
-			<div class="large-8 columns">
+			<div class="large-10 columns">
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, unde, eligendi odit harum mollitia quibusdam aut asperiores dolorem error sed obcaecati atque laudantium vero provident vitae quam repudiandae voluptatem optio.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, eveniet, iusto, doloribus molestiae vero illum magni unde enim ut blanditiis similique voluptatum ducimus voluptates totam officiis quasi quos esse ab!
 			</div>
 		</div>
 		<footer class="row">
